72ea4458c20508cfbc3c6c1f726b098b0d136140
George Irons <georgeirons@Georges-MacBook-Pro.local>
2024-05-23 10:49:12 +0100

    First commit

---
+++ src/main/java/com/appress/hello_rest/HelloWorldRestApplication.java
@@ -0,0 +1,13 @@
+package com.appress.hello_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestApplication.class, args);
+	}
+
+}
